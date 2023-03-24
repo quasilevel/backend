b41e5e1d5229782ab48666f261b48739dd5b11d0
chaoticCalm <vaishalisaini0203@gmail.com>
2023-03-24 22:40:01 +0530

    ...

--- backend/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('user/', views.user,name='user'),
-    path('task/', views.task,name='task'),
-]
